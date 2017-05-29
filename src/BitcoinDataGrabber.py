7487bd3035c97299fd8f6816ebadc663bdfd32a5
redspidermkv <redspidermkv@hotmail.com>
2017-05-29 21:45:40 +0100

    adding eod data

--- src/BitcoinDataGrabber.py
+++ src/BitcoinDataGrabber.py
@@ -4,24 +4,39 @@ import urllib2
 
 apiCurrencyList = 'http://api.coindesk.com/v1/bpi/supported-currencies.json'
 apiCurrentPrice = 'http://api.coindesk.com/v1/bpi/currentprice.json'
+apiCurrentPriceCode = 'http://api.coindesk.com/v1/bpi/currentprice/{}.json'
+apiEODPrice = 'http://api.coindesk.com/v1/bpi/historical/close.json?start={}&end={}&currency={}' # yyyy-mm-dd
 
 def GetApiJsonResponse(url):
 	response = urllib2.urlopen(url)
-	
 	data = json.load(response)
 	return data
 
-jsonData = GetApiJsonResponse(apiCurrencyList)
+def GetApiRawResponse(url):
+	response = urllib2.urlopen(url)
+	data = response.read()
+	return data
 
 def printSupportedCurrencies():
+	jsonData = GetApiJsonResponse(apiCurrencyList)
 	indexSymbol = 'currency'
 	indexCurrency = 'country'
 	for data in jsonData:
+		dataStr = ''
 		try:
 			dataStr = '{}\t{}'.format(data[indexSymbol], data[indexCurrency])
-			print dataStr
 		except Exception, e:
-			print data
+			dataStr = '{}\t{}'.format(data[indexSymbol], '')
+			
+		print dataStr
+	
+def printEODData(startDate, endDate, currency):
+	jsonData = GetApiJsonResponse(apiEODPrice.format(startDate, endDate, currency))
+	eodData = jsonData['bpi']
+	
+	for data in eodData:
+		print data, eodData[data]
 			
 if __name__ == '__main__':
-	printSupportedCurrencies()
+	#printSupportedCurrencies(jsonData)
+	printEODData('2017-05-01', '2017-05-26', 'GBP')
